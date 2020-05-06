cb33f2b2facf9fc46bd2fc6f9dc606a6f168e270
Natalman <natalnahm@gmail.com>
2020-05-06 16:15:04 -0500

    Make the checkbosx functionality work and change its state to stateless widget.

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('this is a task.'),
-      trailing: Checkbox(value: false,
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+
+  final bool checkboxState;
+  TaskCheckbox({this.checkboxState})
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxState,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: (newValue){
+//        setState(() {
+//          isChecked = newValue;
+//        });
+      },
     );
   }
 }
