3e3268a25f51cbc713bd6de3b48da29737259efe
Natalman <natalnahm@gmail.com>
2020-05-08 11:44:41 -0500

    Now we can remove task onlongppresed.

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -17,6 +17,9 @@ class TaskList extends StatelessWidget {
               checkboxCallback: (bool checkboxState){
                 taskData.updateTask(task);
               },
+              onLongPressed: (){
+                taskData.deleteTask(task);
+              },
             );
           },
           itemCount: taskData.taskCount,
--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -5,12 +5,14 @@ class TaskTile extends StatelessWidget {
   bool isChecked = false;
   final String taskTitle;
   final Function checkboxCallback;
+  final Function onLongPressed;
 
-  TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback});
+  TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback, this.onLongPressed});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onLongPress: onLongPressed,
       title: Text(
         taskTitle,
         style: TextStyle(
